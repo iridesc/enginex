98cbdf244337831e56bb44d5464c69c01d7ca236
iridesc <lielang6@live.com>
2019-10-11 18:07:02 +0800

    优化

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import json
 
 def config():
-    print('enginex config done!')
     data=json.load(open('setting.json'))
 
     for key,value in data.items():
-        v=input('please input{} (default:{}):'.format(key,value))
+        v=input('please input {} (current:{}):'.format(key,value))
         if v!='':
-            data[key]=v
-    json.dump(data,open('setting.json','w'),)
+            data[key] = v
+            
+    json.dump(data,open('setting.json','w'),ensure_ascii=False,indent=4)
     print('enginex config done!')
+
 if __name__ == '__main__':
-    config
+    config()
