ee0631c35083832d356b090d7c7c915cf7459235
iridesc <lielang6@live.com>
2019-10-30 14:36:46 +0800

    modify RawRes

--- enginex_4_1.py
+++ enginex_4_1.py
@@ -20,52 +20,56 @@ class RawRes:
         self.weblink = weblink
         self.type = restype
         self.keyword = keyword
+        self.parsedlink = reslink
         self.filename = None
         self.filesize = 0
 
     def reslinkparser(self):
-        def base64decode(link):
-            link = link.split('//')[1]
+        def base64decode():
+            # 获取到被编码的部分
+            link = self.parsedlink.split('/')[-1]
+            # b64解码 得到byte
             example = base64.b64decode(link)
             for i in ['utf-8', 'gbk', 'ascii', 'gb2312', 'GB18030', 'iso8859-1', 'utf-16', ]:
                 try:
                     example = example.decode(encoding=i)[2:][:-2]
-                    return example
+                    self.parsedlink = example
+                    break
                 except:
                     pass
 
-        parselink = parse.unquote(self.reslink)
-        t = parselink.split(':')[0]
-        filename = None
-        size = 0
+        # url解码
+        self.parsedlink = parse.unquote(self.parsedlink)
+        # 提取链接类型
+        t = self.parsedlink.split(':')[0]
         try:
             if t == 'http':
-                filename = parselink.split('/')[-1].split('?')[0]
+                self.filename = self.parsedlink.split('/')[-1].split('?')[0]
             elif t == 'ftp':
-                filename = parselink.split('/')[-1]
+                self.filename = self.parsedlink.split('/')[-1]
             elif t == 'magnet':
-                if '&amp;' in parselink:
-                    infolist = parselink.split('&amp;')
+                # 提取参数字符串
+                if '&amp;' in self.parsedlink:
+                    infolist = self.parsedlink.split('&amp;')
                 else:
-                    infolist = parselink.split('&')
+                    infolist = self.parsedlink.split('&')
+                # 提取文件名和文件大小
                 for info in infolist:
                     if info.split('=')[0] == 'dn':
-                        filename = info.split('=')[1]
+                        self.filename = info.split('=')[1]
                     elif info.split('=')[0] == 'xl':
-                        size = int(info.split('=')[1])
+                        self.filesize = int(info.split('=')[1])/1024**2
             elif t == 'ed2k':
-                infolist = parselink.split('|')
-                filename = infolist[2]
-                size = int(infolist[3])
+                infolist = self.parsedlink.split('|')
+                self.filename = infolist[2]
+                self.filesize = int(infolist[3])/1024**2
             elif t == 'thunder':
-                filename, size = reslinkparser(base64decode(parselink))
+                base64decode()
+                self.reslinkparser()
             else:
-                makelog('Unknow Res Type:{}'.format(t))
-
-        except:
+                pass
+        except Exception as e:
             pass
-        self.filename = filename
-        self.filesize = size / 1024 ** 2
 
 
 class Task:
